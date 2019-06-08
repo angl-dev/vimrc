54dbfcd2623759b870d1de218133c7aee74d2091
Ang Li <leon575777642@gmail.com>
2019-06-07 20:47:38 -0700

    Different tmux.conf for local and remote

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ~
+case $1 in
+    --remote)
+        cp ${DIR}/.remote.tmux.conf .tmux.conf
+        ;;
+    --local)
+        cp ${DIR}/.local.tmux.conf .tmux.conf
+        ;;
+    *)
+        echo "ERROR: use --remote or --local to specify if you're configuring a local machine or a server."
+        exit 1
+        ;;
+esac
 if [ ! -d ./.vim/bundle ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
 fi
 cp ${DIR}/.vimrc .
 vim +PluginInstall +qall
-cp ${DIR}/.tmux.conf .
 rm -rf ${DIR}
