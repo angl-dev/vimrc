60c37be85a7516e805fa8c71d1ed1d3e606eea6d
Ang Li <leon575777642@gmail.com>
2019-06-07 19:15:17 -0700

    tmux configuration file added

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-here=`pwd`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ~
 if [ ! -d ./.vim/bundle ]; then
     git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
 fi
-cp ${here}/.vimrc .
+cp ${DIR}/.vimrc .
 vim +PluginInstall +qall
-rm -rf ${here}
+cp ${DIR}/.tmux.conf .
+rm -rf ${DIR}
